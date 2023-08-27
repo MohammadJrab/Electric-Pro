5408642d25b683e5b6aaddfc14e26d10adbb1b52
Mohammad Jrab <mo.jrab02@gmail.com>
2023-08-27 13:04:50 +0300

    Refactor code

--- ElectricPro/Connection.cs
+++ ElectricPro/Connection.cs
@@ -11,8 +11,8 @@ namespace ElectricPro
     {
         // Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.128.0.20)(PORT=1521))) (CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=cluster)));User Id=mjrsy;Password=admin;
         //Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));User Id = mjrsy; Password=admin;Connection Timeout =1
-        public static String ConnectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.128.0.20)(PORT=1521))) (CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=cluster)));User Id=mjrsy;Password=admin;Connection Timeout =1";
-        //public static String ConnectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));User Id = mjrsy; Password=admin;Connection Timeout =1";
-
+        //public static String ConnectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=10.128.0.20)(PORT=1521))) (CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=cluster)));User Id=mjrsy;Password=admin;Connection Timeout =1";
+        public static String ConnectionString = "Data Source=(DESCRIPTION=(ADDRESS_LIST=(ADDRESS=(PROTOCOL=TCP)(HOST=localhost)(PORT=1521)))(CONNECT_DATA=(SERVER=DEDICATED)(SERVICE_NAME=ORCL)));User Id = mjrsy; Password=admin;Connection Timeout =1";
+         
     }
 }
